b4962558d773f0dafe7ae9ad0b48adbd8f3f4df0
Jake Nguyen <jakey@Jakes-Air.fios-router.home>
2020-10-21 17:23:51 -0400

    backend server setup

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const { PORT = 4000, NODE_ENV = 'development' } = process.env
+const mongoose = require('./db/connection')
+
+const cors = require('cors')
+const corsOptions = require('./configs/cors')
+
+const express = require('express')
+const app = express()
+
+const morgan = require('morgan')
+
+NODE_ENV === "production" ? app.use(cors(corsOptions)) : app.use(cors())
+app.use(cors())
+app.use(express.json())
+app.use(morgan('tiny'))
+
+app.get('/', (req, res) => {
+	res.json({ hello: 'Hello World' })
+})
+
+app.listen(PORT, () => {
+	console.log(`You are listening on port ${PORT}`)
+})
